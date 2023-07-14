7ed8df819607d3f7c73ae5eb1076ee8f0a241f60
BalaVignesh <balavignesh@techmango.net>
2023-07-14 10:10:07 +0530

    orderby using asc and desc

---
+++ src/main/java/com/tm/querybuilder/pojo/OrderByPOJO.java
@@ -0,0 +1,24 @@
+package com.tm.querybuilder.pojo;
+
+public class OrderByPOJO {
+
+	private String orderColumnName;
+	private String orderType;
+
+	public String getOrderColumnName() {
+		return orderColumnName;
+	}
+
+	public String getOrderType() {
+		return orderType;
+	}
+
+	public void setOrderColumnName(String orderColumnName) {
+		this.orderColumnName = orderColumnName;
+	}
+
+	public void setOrderType(String orderType) {
+		this.orderType = orderType;
+	}
+
+}
